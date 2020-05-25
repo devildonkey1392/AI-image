bd677182a4e476435e0506c9b36abb2a8774b44b
106062239 <sogoodme40@gmail.com>
2020-05-25 21:37:34 +0800

    only intact image

--- mango.py
+++ mango.py
@@ -50,7 +50,6 @@ for i in range(len(labels_level)):
     labels_level[i] = labels_level[i].replace("A","0")
     labels_level[i] = labels_level[i].replace("B","1")
     labels_level[i] = labels_level[i].replace("C","2")
-
 # 隨機讀取圖片
 a = 0
 items= []
@@ -58,15 +57,11 @@ import random
 for a in range(0,picnum):
     items.append(a)
 
-mode = 1 
-# 1. only flip
-# 2. flip + continuous rotate 
-# 3. flip + independent rotate 
 # 製作訓練用資料集及標籤
 for i in random.sample(items,picnum):
     img = cv2.imread(train_img_dir + labels_pic[i] + ".jpg")
     res = cv2.resize(img,(224,224),interpolation=cv2.INTER_LINEAR)
-
+    res = cv2.cvtColor(res,cv2.COLOR_BGR2RGB)
     # flip image
     img_flip0=cv2.flip(res,0)
     img_flip1=cv2.flip(res,1)
@@ -77,63 +72,16 @@ for i in random.sample(items,picnum):
     # rotate image
     (h, w) = res.shape[:2] 
     center = (w // 2, h // 2)
-    if mode==2:
-      M = cv2.getRotationMatrix2D(center, 30, 1.0)
-      M2 = cv2.getRotationMatrix2D(center, 60, 1.0)  
-      rotated_img_30 = cv2.warpAffine(res, M, (w, h))
-      rotated_img_60 = cv2.warpAffine(rotated_img_30, M, (w, h))
-      rotated_img_120 = cv2.warpAffine(rotated_img_60, M2, (w, h))
-      rotated_img_150 = cv2.warpAffine(rotated_img_120, M, (w, h))
-      rotated_img_210 = cv2.warpAffine(rotated_img_150, M2, (w, h))
-      rotated_img_240 = cv2.warpAffine(rotated_img_210, M, (w, h))
-      rotated_img_300 = cv2.warpAffine(rotated_img_240, M2, (w, h))
-      rotated_img_330 = cv2.warpAffine(rotated_img_300, M, (w, h))
-      X.append(img_to_array(rotated_img_30))
-      X.append(img_to_array(rotated_img_60))
-      X.append(img_to_array(rotated_img_120))
-      X.append(img_to_array(rotated_img_150))
-      X.append(img_to_array(rotated_img_210))
-      X.append(img_to_array(rotated_img_240))
-      X.append(img_to_array(rotated_img_300))
-      X.append(img_to_array(rotated_img_330))
-    elif mode==3:
-      M = []
-      M.append(cv2.getRotationMatrix2D(center, 30, 1.0))
-      M.append(cv2.getRotationMatrix2D(center, 60, 1.0))
-      M.append(cv2.getRotationMatrix2D(center, 120, 1.0))
-      M.append(cv2.getRotationMatrix2D(center, 150, 1.0))
-      M.append(cv2.getRotationMatrix2D(center, 210, 1.0))
-      M.append(cv2.getRotationMatrix2D(center, 240, 1.0))
-      M.append(cv2.getRotationMatrix2D(center, 300, 1.0))
-      M.append(cv2.getRotationMatrix2D(center, 330, 1.0))
-
-      rotated_img_30 = cv2.warpAffine(res, M[0], (w, h))
-      rotated_img_60 = cv2.warpAffine(res, M[1], (w, h))
-      rotated_img_120 = cv2.warpAffine(res, M[2], (w, h))
-      rotated_img_150 = cv2.warpAffine(res, M[3], (w, h))
-      rotated_img_210 = cv2.warpAffine(res, M[4], (w, h))
-      rotated_img_240 = cv2.warpAffine(res, M[5], (w, h))
-      rotated_img_300 = cv2.warpAffine(res, M[6], (w, h))
-      rotated_img_330 = cv2.warpAffine(res, M[7], (w, h))
-      X.append(img_to_array(rotated_img_30))
-      X.append(img_to_array(rotated_img_60))
-      X.append(img_to_array(rotated_img_120))
-      X.append(img_to_array(rotated_img_150))
-      X.append(img_to_array(rotated_img_210))
-      X.append(img_to_array(rotated_img_240))
-      X.append(img_to_array(rotated_img_300))
-      X.append(img_to_array(rotated_img_330))
-    if mode==1:
-      for j in range(3):
-        y.append(labels_level[i])
-    else:
-      for j in range(11):
-        y.append(labels_level[i])
-    
+    M_90 = cv2.getRotationMatrix2D(center, 90, 1.0)
+    M_270 = cv2.getRotationMatrix2D(center, 270, 1.0)  
+    rotated_img_30 = cv2.warpAffine(res, M_90, (w, h))
+    rotated_img_270 = cv2.warpAffine(res, M_270, (w, h))
+    X.append(img_to_array(rotated_img_30))
+    X.append(img_to_array(rotated_img_270))
     res = img_to_array(res)
     X.append(res)    
-    y.append(labels_level[i])
-
+    for j in range(6):
+        y.append(labels_level[i])
 
 y_label_org = y
 print(y)
@@ -145,17 +93,9 @@ y = np.array(y)
 # 轉換至float的格式
 for i in range(len(X)):
     X[i] = X[i].astype('float32')
-# 打映圖片訓練集的概況
-# print(X[0])
-print(type(X))
-print(X.shape)
 
-print(X[0].shape)
-print(type(X[0]))
 # 將標籤轉換至float格式
 y = tf.strings.to_number(y, out_type=tf.float32)
-# 打映圖片標籤的概況
-print(y[0])
-print(type(y[0]))
+
 # 標籤進行one-hotencoding
 y = np_utils.to_categorical(y, num_classes = 3)
