7b8406293dfb0df809936ac8e097fffb81683fb2
devildonkey1392 <56789079+devildonkey1392@users.noreply.github.com>
2020-06-11 18:36:42 +0800

    update

--- GAN.py
+++ GAN.py
@@ -40,39 +40,45 @@ print("芒果圖片數量: ",picnum)
 train_images = []
 
 for i in range(len(labels_pic)):
-  if labels_level[i]=="A":
+  if labels_level[i]=="C":
+
     img = io.imread(train_img_dir + labels_pic[i])
     #img = cv2.imread(train_img_dir + labels_pic[i])
-    res = cv2.resize(img,(112,112),interpolation=cv2.INTER_LINEAR)
-    #res = cv2.cvtColor(res,cv2.COLOR_BGR2RGB)
-    #if i < 10:
-        #plt.imshow(res)
-        #plt.axis("off")
-        #plt.show()
+    img = cv2.resize(img,(128,128),interpolation=cv2.INTER_LINEAR)
+    res = img[:, :, [2, 1, 0]]
     # flip image
     img_flip0=cv2.flip(res,0)
     img_flip1=cv2.flip(res,1)
     img_flip2=cv2.flip(res,-1)
-    train_images.append(img_to_array(img_flip0))
-    train_images.append(img_to_array(img_flip1))
-    train_images.append(img_to_array(img_flip2))
+    img_flip0 = img_flip0[:, :, [2, 1, 0]]
+    img_flip1 = img_flip1[:, :, [2, 1, 0]]
+    img_flip2 = img_flip2[:, :, [2, 1, 0]]
+    train_images.append(np.asarray(img_flip0))
+    train_images.append(np.asarray(img_flip1))
+    train_images.append(np.asarray(img_flip2))
     # rotate image
     (h, w) = res.shape[:2] 
     center = (w // 2, h // 2)
     M_90 = cv2.getRotationMatrix2D(center, 90, 1.0)
     M_270 = cv2.getRotationMatrix2D(center, 270, 1.0)  
-    rotated_img_30 = cv2.warpAffine(res, M_90, (w, h))
+    rotated_img_90 = cv2.warpAffine(res, M_90, (w, h))
     rotated_img_270 = cv2.warpAffine(res, M_270, (w, h))
-    train_images.append(img_to_array(rotated_img_30))
-    train_images.append(img_to_array(rotated_img_270))
-    res = img_to_array(res)
-    train_images.append(res)
+    rotated_img_90 = rotated_img_90[:, :, [2, 1, 0]]
+    rotated_img_270 = rotated_img_270[:, :, [2, 1, 0]]
+    train_images.append(np.asarray(rotated_img_90))
+    train_images.append(np.asarray(rotated_img_270))
+    res = res[:, :, [2, 1, 0]]
+    if i < 5:
+        plt.imshow(res)
+        plt.show()
+    train_images.append(np.asarray(res))
 
 train_images = np.array(train_images)
 print(train_images.shape)
+
 datagen = ImageDataGenerator(
     zca_whitening=False,
-    rotation_range=40,
+    rotation_range=20,
     width_shift_range=0.2,
     height_shift_range=0.2,
     shear_range=0.2,
@@ -81,10 +87,11 @@ datagen = ImageDataGenerator(
     fill_mode='nearest')
 datagen.fit(train_images)
 
+plt.imshow(train_images[0])
+plt.show()
 #(train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
-train_images = train_images.reshape(train_images.shape[0], 112, 112, 3).astype('float32')
+train_images = train_images.reshape(train_images.shape[0], 128, 128, 3).astype('float32')
 train_images = (train_images - 127.5) / 127.5 # 将圖片normalize到[-1, 1]
-print(len(train_images))
 BUFFER_SIZE = len(train_images)
 BATCH_SIZE = 128
 
@@ -94,7 +101,7 @@ train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_
 # 生成器
 def make_generator_model():
     model = tf.keras.Sequential()
-    size = 7
+    size = 8
     model.add(layers.Dense(size*size*512, use_bias=False, input_shape=(100,)))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
@@ -125,13 +132,15 @@ def make_generator_model():
 generator = make_generator_model()
 noise = tf.random.normal([1, 100])
 generated_image = generator(noise, training=False)
-plt.imshow(generated_image[0, :, :, 0], cmap="gray")
+print(generated_image.shape)
+plt.imshow(generated_image[0]*127.5 + 127.5)
+plt.show()
 
 # 判別器
 def make_discriminator_model():
     model = tf.keras.Sequential()
     model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
-                                     input_shape=[112, 112, 3]))
+                                     input_shape=[128, 128, 3]))
     model.add(layers.LeakyReLU())
     model.add(layers.Dropout(0.3))
 
@@ -169,14 +178,14 @@ def generator_loss(fake_output):
 generator_optimizer = tf.keras.optimizers.Adam(1e-4)
 discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
 
-checkpoint_dir = './checkpoint'
+checkpoint_dir = './checkpoint_C'
 checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
 checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
                                  discriminator_optimizer=discriminator_optimizer,
                                  generator=generator,
                                  discriminator=discriminator)
 
-EPOCHS = 50
+EPOCHS = 1000
 noise_dim = 100
 num_examples_to_generate = 16
 
@@ -212,6 +221,7 @@ def train(dataset, epochs):
 
     # 继续进行时为 GIF 生成图像
     display.clear_output(wait=True)
+    seed = tf.random.normal([num_examples_to_generate, noise_dim])
     generate_and_save_images(generator,
                              epoch + 1,
                              seed)
@@ -234,13 +244,22 @@ def generate_and_save_images(model, epoch, test_input):
   predictions = model(test_input, training=False)
 
   fig = plt.figure(figsize=(4,4))
+  
+  for i in range(predictions.shape[0]):
+      plt.subplot(4, 4, i+1)
+      plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+      plt.axis('off')
+  plt.show()
+      
 
   for i in range(predictions.shape[0]):
       plt.subplot(4, 4, i+1)
-      plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap="gray")
-      #plt.axis('off')
-  #cmap='gray'
-  plt.savefig('GAN_IMAGE/image_at_epoch_{:04d}.png'.format(epoch))
+      img = predictions[i]* 127.5 + 127.5
+      img = array_to_img(img)
+      plt.imshow(img)
+      plt.axis('off')
+
+  plt.savefig('GAN_IMAGE_C/image_at_epoch_{:04d}.png'.format(epoch))
   plt.show()
 
 # 恢復最新的checkpoint
@@ -250,5 +269,5 @@ train(train_dataset, EPOCHS)
 
 # 使用 epoch 数生成单张图片
 def display_image(epoch_no):
-  return PIL.Image.open('GAN_IMAGE/image_at_epoch_{:04d}.png'.format(epoch_no))
+  return PIL.Image.open('GAN_IMAGE_C/image_at_epoch_{:04d}.png'.format(epoch_no))
 display_image(EPOCHS)
